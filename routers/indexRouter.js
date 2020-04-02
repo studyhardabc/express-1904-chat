ae3b2f429160ff4e90a6177f1e8077a04b506421
studyhardabc <974224401@qq.com>
2020-04-02 20:22:24 +0800

    init

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,29 +1,19 @@
-const express = require("express");
-const auth = require("../middlewares/auth");
+const express = require('express');
 const router = express.Router();
 
-// GET / 欢迎页面
-router.get("/", (req, res) => {
-  console.log(req.session);
+//GET / 欢迎页面
+router.get('/', (req,res) => {
+    res.render('welcome.ejs');
+})
 
-  res.render("welcome");
-});
+//GET /chatroom 聊天室页面
+router.get('/chatroom', (req,res) => {
+    res.render('chatroom.ejs');
+})
 
-// GET /chatroom 聊天室页面
-router.get("/chatroom", auth, (req, res) => {
-  res.render("chatroom", { username: req.auth.username });
-});
-
-// GET /posts 帖子列表页面
-router.get("/posts", auth, (req, res) => {
-  res.render("post/index");
-});
-
-// GET /login 登录页面
-router.get("/login", (req, res) => {
-  // 可以获取 ？号传参
-  const redirect = req.query.redirect || "/";
-  res.render("login", { redirect });
-});
+//GET /login 登录页面
+router.get('/login', (req,res) => {
+    res.render('login');
+})
 
 module.exports = router;
